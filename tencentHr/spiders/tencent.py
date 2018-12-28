b3bbd6d614ed7b22c2f96ec69492a8957f91bcfe
caiwenming <caiwenming@myhexin.com>
2018-12-28 09:27:30 +0800

    format

--- tencentHr/spiders/tencent.py
+++ tencentHr/spiders/tencent.py
@@ -10,24 +10,24 @@ class TencentSpider(scrapy.Spider):
 
     def parse(self, response):
         for each in respose.xpath('//*[@class="even"]'):
-			item = TencentItem()
-			name = each.xpath('./td[1]/a/text()').extrace()[0]
-			detailLink = each.xpath('./td[1]/a/@href').extrace()[0]
-			positionInfo = each.xpath('./td[2]/text()').extrace()[0]
-			peopleNumber = each.xpath('./td[3]/text()').extrace()[0]
-			workLocation = each.xpath('./td[4]/text()').extrace()[0]
-			publishTime = each.xpath('./td[5]/text()').extrace()[0]
+            item = TencentItem()
+            name = each.xpath('./td[1]/a/text()').extrace()[0]
+            positionInfo = each.xpath('./td[2]/text()').extrace()[0]
+            peopleNumber = each.xpath('./td[3]/text()').extrace()[0]
+            workLocation = each.xpath('./td[4]/text()').extrace()[0]
+            publishTime = each.xpath('./td[5]/text()').extrace()[0]
 
-			item['name'] = name.encode('utf-8')
-			item['detailLink'] = detailLink.encode('utf-8')
-			item['positionInfo'] = positionInfo.encode('utf-8')
-			item['peopleNumber'] = peopleNumber.encode('utf-8')
-			item['workLocation'] = workLocation.encode('utf-8')
-			item['publishTime'] = publishTime.encode('utf-8')
 
-		curpage = re.search('(\d+)', response.url).group(1)
-		page = int(curpage) + 10
-		url = re.sub('+\d', str(page), response.url)
-		yield scrapy.Request(url, callback = self.parse)
-		yield item
+            item['name'] = name.encode('utf-8')
+            item['detailLink'] = detailLink.encode('utf-8')
+            item['positionInfo'] = positionInfo.encode('utf-8')
+            item['peopleNumber'] = peopleNumber.encode('utf-8')
+            item['workLocation'] = workLocation.encode('utf-8')
+            item['publishTime'] = publishTime.encode('utf-8')
+
+            curpage = re.search('(\d+)', response.url).group(1)
+            page = int(curpage) + 10
+            url = re.sub('+\d', str(page), response.url)
+            yield scrapy.Request(url, callback = self.parse)
+            yield item
 
